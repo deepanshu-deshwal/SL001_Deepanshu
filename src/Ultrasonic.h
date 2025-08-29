12cdb46b032bf4f73fa3f102fc9e6e61a5ccbe85
Deepanshu Deshwal <99385437+deepanshu-deshwal@users.noreply.github.com>
2025-08-29 11:55:02 +0530

    Update and rename Ultrasonic.h to Ultrasonic_Deepanshu.h

--- src/Ultrasonic.h
+++
@@ -1,5 +0,0 @@
-#ifndef ULTRASONIC_H
-#define ULTRASONIC_H
-#include <Arduino.h>
-float getDistance(int trig, int echo);
-#endif
---
+++ src/Ultrasonic_Deepanshu.h
@@ -0,0 +1,5 @@
+#ifndef ULTRASONIC_DEEPANSHU_H
+#define ULTRASONIC_DEEPANSHU_H
+#include <Arduino.h>
+float getDistance(int trig, int echo);
+#endif
