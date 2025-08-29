20721acc3d9a1a67a0765c801487c5c138acc4c9
Deepanshu Deshwal <99385437+deepanshu-deshwal@users.noreply.github.com>
2025-08-29 18:46:02 +0530

    Update Ultrasonic_Deepanshu.h

--- src/Ultrasonic_Deepanshu.h
+++ src/Ultrasonic_Deepanshu.h
@@ -1,5 +1,5 @@
-#ifndef ULTRASONIC_DEEPANSHU_H
-#define ULTRASONIC_DEEPANSHU_H
+#ifndef SL001_DEEPANSHU_H
+#define SL001_DEEPANSHU_H
 #include <Arduino.h>
 float getDistance(int trig, int echo);
 #endif
